c83bfa37ecf8659cf46ac01019f7468ba86c3940
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-07-29 16:14:55 +0300

    feat(md5): read file/stdin, send data to md5 mock

--- src/md5.rs
+++ src/md5.rs
@@ -1,40 +1,46 @@
+use anyhow::Context as Ctx;
+use anyhow::Result;
 use clap::Args;
-use std::{
-    fmt,
-    io::{self, Write},
-    path::PathBuf,
-};
+use std::{fmt, fs, io, path};
 
 #[derive(Args)]
 pub struct MD5 {
     /// Files to digest (optional; default is stdin).
     /// With no FILE, or when FILE is -, read standard input.
-    file: Option<Vec<PathBuf>>,
+    file: Option<Vec<path::PathBuf>>,
 }
 
 impl MD5 {
-    pub fn exec(&self) {
+    pub fn exec(&self) -> Result<()> {
+        macro_rules! print_hash {
+            ( $name:expr, $no_buf_reader:expr ) => {
+                let mut buf_r = io::BufReader::new($no_buf_reader);
+                let mut hasher = Context::new();
+                io::copy(&mut buf_r, &mut hasher).with_context(|| "could not read data")?;
+                println!("{} {}", hasher.compute(), $name);
+            };
+        }
         match &self.file {
             Some(files) => {
                 for file in files.iter() {
-                    process_file(file)
+                    let name = file.to_str().unwrap_or("-");
+                    if name == "-" {
+                        print_hash!("-", io::stdin());
+                        continue;
+                    }
+                    print_hash!(
+                        name,
+                        fs::File::open(file)
+                            .with_context(|| format!("could not open file `{}`", name))?
+                    );
                 }
             }
-            None => process_file(&PathBuf::from("-")),
+            None => {
+                print_hash!("-", io::stdin());
+            }
         };
+        Ok(())
     }
-}
-
-fn process_file(file: &PathBuf) {
-    let data = read_file(file);
-    let mut ctx = Context::new();
-    let _ = ctx.write(&data);
-
-    println!("{} {}", ctx.compute(), file.display())
-}
-
-fn read_file(_file: &PathBuf) -> Vec<u8> {
-    unimplemented!()
 }
 
 pub struct Digest(pub [u8; 16]);
@@ -52,26 +58,29 @@ impl io::Write for Context {
 }
 
 impl fmt::Display for Digest {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        unimplemented!()
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // TODO: implement me
+        write!(f, "implement_me")
     }
 }
 
 impl Context {
     /// Create new Context to md5 hash calculation, with initial values.
     pub fn new() -> Context {
-        unimplemented!()
+        // TODO: implement me
+        Context {}
     }
     /// Add last md5 word to data (padding and length of data), consume it and then
     /// return state (hash) of the Context.
     pub fn compute(self) -> Digest {
-        unimplemented!()
+        // TODO: implement me
+        Digest([0; 16])
     }
 }
 
 impl Context {
     /// Consume data, calculate new state for each md5 word (512 bits).
     fn consume(&mut self, _data: &[u8]) {
-        unimplemented!()
+        // TODO: implement me
     }
 }
