96ffa99e500a8753d46456bc669169fd2cfd7142
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-09-24 09:10:48 +0300

    feat(sha256): implement SHA256 algorithm
    Close #18

---
+++ src/hasher.rs
@@ -0,0 +1,122 @@
+use std::io::{self, Write};
+
+const CHUNK_BYTE_SIZE: usize = 64;
+const PADDING: [u8; 64] = [
+    0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+];
+
+const DATA_BITS_LENGTH_BYTE_SIZE: usize = 8;
+const END_OF_DATA_BYTE_SIZE: usize = 1;
+
+pub trait Context {
+    type Digest;
+
+    fn compress(&mut self, chunk: &[u8; CHUNK_BYTE_SIZE]);
+    fn get_digest(self) -> Self::Digest;
+}
+
+pub enum Endian {
+    Big,
+    Little,
+}
+
+pub struct Writer<Ctx: Context> {
+    buf: [u8; CHUNK_BYTE_SIZE],
+    buf_seed: usize,
+    data_bytes_len: usize,
+    endian: Endian,
+    hasher: Ctx,
+}
+
+impl<Ctx: Context> Write for Writer<Ctx> {
+    fn write(&mut self, mut buf: &[u8]) -> io::Result<usize> {
+        self.data_bytes_len = self.data_bytes_len.wrapping_add(buf.len());
+
+        while self.buf_seed + buf.len() > CHUNK_BYTE_SIZE {
+            self.buf[self.buf_seed..CHUNK_BYTE_SIZE]
+                .clone_from_slice(&buf[..CHUNK_BYTE_SIZE - self.buf_seed]);
+            self.hasher.compress(&self.buf);
+            buf = &buf[CHUNK_BYTE_SIZE - self.buf_seed..];
+            self.buf_seed = 0;
+        }
+        self.buf[self.buf_seed..self.buf_seed + buf.len()].clone_from_slice(buf);
+        self.buf_seed += buf.len();
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl<Ctx: Context> Writer<Ctx> {
+    pub fn new(hasher: Ctx, endian: Endian) -> Writer<Ctx> {
+        Writer {
+            buf: [0; CHUNK_BYTE_SIZE],
+            buf_seed: 0,
+            data_bytes_len: 0,
+            hasher,
+            endian,
+        }
+    }
+
+    pub fn compute(mut self) -> Ctx::Digest {
+        let data_bits_len = (self.data_bytes_len as u64).wrapping_mul(8);
+        // check self.buf_seed
+        // if buf_seed > 64 - 9 => two final chunks
+        // else => one final chunk
+        if self.buf_seed <= CHUNK_BYTE_SIZE - (END_OF_DATA_BYTE_SIZE + DATA_BITS_LENGTH_BYTE_SIZE) {
+            let pading_bytes_len = CHUNK_BYTE_SIZE - DATA_BITS_LENGTH_BYTE_SIZE - self.buf_seed;
+            self.buf[self.buf_seed..self.buf_seed + pading_bytes_len]
+                .clone_from_slice(&PADDING[..pading_bytes_len]);
+            self.fill_data_len(data_bits_len);
+            self.hasher.compress(&self.buf);
+        } else {
+            // chunk 1
+            let pading_bytes_len = CHUNK_BYTE_SIZE - self.buf_seed;
+            self.buf[self.buf_seed..self.buf_seed + pading_bytes_len]
+                .clone_from_slice(&PADDING[..pading_bytes_len]);
+            self.hasher.compress(&self.buf);
+
+            // chunk 2
+            self.buf[..CHUNK_BYTE_SIZE - DATA_BITS_LENGTH_BYTE_SIZE]
+                .clone_from_slice(&PADDING[DATA_BITS_LENGTH_BYTE_SIZE..]);
+            if pading_bytes_len == 0 {
+                self.buf[0] = PADDING[0];
+            }
+            self.fill_data_len(data_bits_len);
+            self.hasher.compress(&self.buf);
+        }
+
+        self.hasher.get_digest()
+    }
+
+    fn fill_data_len(&mut self, bits_len: u64) {
+        match self.endian {
+            Endian::Big => {
+                self.buf[CHUNK_BYTE_SIZE - 1] = (bits_len & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 2] = ((bits_len >> 8) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 3] = ((bits_len >> 16) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 4] = ((bits_len >> 24) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 5] = ((bits_len >> 32) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 6] = ((bits_len >> 40) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 7] = ((bits_len >> 48) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 8] = ((bits_len >> 56) & 0xff) as u8;
+            }
+            Endian::Little => {
+                self.buf[CHUNK_BYTE_SIZE - 8] = (bits_len & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 7] = ((bits_len >> 8) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 6] = ((bits_len >> 16) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 5] = ((bits_len >> 24) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 4] = ((bits_len >> 32) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 3] = ((bits_len >> 40) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 2] = ((bits_len >> 48) & 0xff) as u8;
+                self.buf[CHUNK_BYTE_SIZE - 1] = ((bits_len >> 56) & 0xff) as u8;
+            }
+        }
+    }
+}
--- src/helper.rs
+++ src/helper.rs
@@ -25,3 +25,44 @@ impl<'a> Read for Input<'a> {
         }
     }
 }
+
+pub fn as_u8_be(x: u32) -> [u8; 4] {
+    let mut bytes = [0u8; 4];
+
+    bytes[3] = (x & 0xff) as u8;
+    bytes[2] = ((x >> 8) & 0xff) as u8;
+    bytes[1] = ((x >> 16) & 0xff) as u8;
+    bytes[0] = ((x >> 24) & 0xff) as u8;
+
+    bytes
+}
+
+pub fn as_u32_be(bytes: &[u8]) -> u32 {
+    ((bytes[0] as u32) << 24)
+        + ((bytes[1] as u32) << 16)
+        + ((bytes[2] as u32) << 8)
+        + ((bytes[3] as u32) << 0)
+}
+
+pub fn right_rotate(x: u32, s: usize) -> u32 {
+    (x >> s) | (x << (32 - s))
+}
+
+mod tests {
+    use super::*;
+    macro_rules! right_rotate_test {
+        ($name:ident,$expected:expr,$data:expr,$shift:expr) => {
+            #[test]
+            fn $name() {
+                let actual = right_rotate($data, $shift);
+
+                println!("  actual: {:X?}", actual);
+                println!("expected: {:X?}", $expected);
+                assert_eq!($expected, actual);
+            }
+        };
+    }
+
+    right_rotate_test!(rr_byte_shift, 0xffff00ffu32, 0xff00ffffu32, 8);
+    right_rotate_test!(rr_bits_1, 0xffff00ffu32, 0xff00ffffu32, 8);
+}
